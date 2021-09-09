a394944858683a85f18523d9204da149ed4ce683
ayrtonbolanos <ayrtonbolanos.anbo@gmail.com>
2021-09-08 20:17:30 -0500

    Create DAODepartamento.java

---
+++ DAODepartamento.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.util.List;
+import POJO.Departamento;
+
+/**
+ *
+ * @author ayrto
+ */
+public class DAODepartamento implements IDAOGeneral <Departamento> {
+
+    @Override
+    public boolean crear(Departamento pojo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean modificar(Departamento pojo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean eliminar(long clave) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Departamento buscarById(long clave) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Departamento> buscarAll(long clave) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+}
