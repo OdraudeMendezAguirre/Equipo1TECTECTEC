8152aab0d54de88ce2a272523fef7b741073cb28
ayrtonbolanos <ayrtonbolanos.anbo@gmail.com>
2021-09-08 20:15:34 -0500

    Create DAOEmpleado.java

---
+++ DAOEmpleado.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.util.List;
+import POJO.Empleado;
+
+/**
+ *
+ * @author ayrto
+ */
+public class DAOEmpleado implements IDAOGeneral <Empleado> {
+
+    @Override
+    public boolean crear(Empleado pojo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean modificar(Empleado pojo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean eliminar(long clave) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Empleado buscarById(long clave) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Empleado> buscarAll(long clave) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+   
+}
