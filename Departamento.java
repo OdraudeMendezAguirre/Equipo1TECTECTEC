90658d1bd120100cded4fdc9b4ca3a29d0bda3ec
ayrtonbolanos <ayrtonbolanos.anbo@gmail.com>
2021-09-08 20:25:36 -0500

    Create Departamento.java

---
+++ Departamento.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package POJO;
+
+/**
+ *
+ * @author ayrto
+ */
+public class Departamento {
+    private int clave;
+    private String nombre;
+    private String direccion;
+
+    public Departamento(int clave, String nombre, String direccion) {
+        this.clave = clave;
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public int getClave() {
+        return clave;
+    }
+
+    public void setClave(int clave) {
+        this.clave = clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
